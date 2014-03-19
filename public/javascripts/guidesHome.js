133dd1fe9484b3438601af011ec1e97670962acb
Jordan Burrows <jordanburrows@gmail.com>
2014-03-19 12:22:32 -0600

    added class to newMessages

--- public/javascripts/guidesHome.js
+++ public/javascripts/guidesHome.js
@@ -31,6 +31,10 @@ $(function(){
 
 
 
+    //on click remove blinking class
+    $(document).on('click', '.messageQueueDesign', function(){
+    	$(this).removeClass('newMessage');
+    })
 	//SOCKET CODE FOR GUIDES
 	// connect the socket.io server
 	var socket = io.connect("http://localhost");
@@ -41,14 +45,14 @@ $(function(){
 	});
 
 	socket.on("message",function(data){
-		// console.log(data);
 		if($('.chatSidebar[data-id='+data.from+']').length < 1){
-			$("#messageQueue").append(chatInboxTemplate({name: data.name, message: data.message, from: data.from, image: data.image}));
+			$("#messageQueue").prepend(chatInboxTemplate({name: data.name, message: data.message, from: data.from, image: data.image}));
 			appendMessages(data);
 			getIncomingMessage(data);
 			$('.chatWindow[data-id='+data.from+']').closest('.touristChatBox').hide();
 		}
 		else{
+			$('#messageQueue').prepend($('.chatSidebar[data-id='+data.from+']').closest('.messageQueueDesign').addClass('newMessage'));
 			getIncomingMessage(data);
 			$('.chatBody').scrollTop($('.chat').height());
 		}
@@ -81,7 +85,6 @@ $(function(){
 	function getIncomingMessage(data){
 		data.timestamp = moment(data.timestamp).format('MMM D, h:mm a');
 		$('.chatWindow[data-id='+data.from+']').find('.chat').append(incomingTemplate(data));
-		// console.log(data)
 	}
 
 	function getOutgoingMessage(data){
@@ -94,7 +97,6 @@ $(function(){
 		if(e.keyCode === 13){
 			var receiverID = $(this).closest('.chatWindow').attr("data-id");
 			var message = $(this).val();
-			// var name = guideData.firstName;
 			var timestamp = moment().format('MMM D, h:mm a YYYY');
 			socket.emit('message',{message: message, timestamp: timestamp, name: guideData.firstName, from: guideData._id, image: guideData.picture, to: receiverID});
 			$(this).closest(".chatWindow").find(".chat").append(outgoingTemplate({timestamp: moment(timestamp).format('MMM D, h:mm a'), name: guideData.firstName, message: message, from: guideData._id, image: guideData.picture}));
@@ -106,7 +108,6 @@ $(function(){
 	$(document).on("click", ".btn-chat" ,function(){
 		var receiverID = $(this).closest('.chatWindow').attr("data-id");
 		var message = $(this).closest('.chatWindow').find(".messageBox").val();
-		// var name = guideData.firstName;
 		var timestamp = moment().format('MMM D, h:mm a YYYY');
 		socket.emit('message',{message: message, timestamp: timestamp, name: guideData.firstName, from: guideData._id, image: guideData.picture, to: receiverID});
 		$(this).closest(".chatWindow").find(".chat").append(outgoingTemplate({timestamp: moment(timestamp).format('MMM D, h:mm a'), name: guideData.firstName, message: message, from: guideData._id, image: guideData.picture}));
@@ -131,35 +132,30 @@ $(function(){
 	})
 	console.log(allMessages)
 	for (var i = 0; i < allMessages.length; i++) {
-		// var receivedMessages = messageData.filter(function(item){
-		// 	return item.from._id !== guideData._id;
-		// });
-
-		// var sentMessages = messageData.filter(function(outgoing){
-		// 	return outgoing.from._id === guideData._id;
-		// });
 		if(allMessages[i].from._id !== guideData._id ){
 			if($('.chatSidebar[data-id='+allMessages[i].from._id+']').length < 1 ){
-					$("#messageQueue").append(chatInboxTemplate({from: allMessages[i].from._id , name: allMessages[i].from.name, image: allMessages[i].from.picture}));
+					$("#messageQueue").prepend(chatInboxTemplate({from: allMessages[i].from._id , name: allMessages[i].from.name, image: allMessages[i].from.picture}));
 					appendMessages({name: allMessages[i].from.name, from: allMessages[i].from._id});
 					getIncomingMessage({name: allMessages[i].from.firstName, from: allMessages[i].from._id, image: allMessages[i].from.picture, message:allMessages[i].message, timestamp: allMessages[i].time});
 					$('.chatWindow[data-id='+allMessages[i].from._id+']').closest('.touristChatBox').hide();
 					console.log(i)
 			}
 			else{
+				$('#messageQueue').prepend($('.chatSidebar[data-id='+allMessages[i].from._id+']').closest('.messageQueueDesign'))
 				getIncomingMessage({name: allMessages[i].from.firstName, from: allMessages[i].from._id, image: allMessages[i].from.picture, message:allMessages[i].message, timestamp: allMessages[i].time});
 				$('.chatBody').scrollTop($('.chat').height());
 			}
 		}
 		else{
 			if($('.chatSidebar[data-id='+allMessages[i].to._id+']').length < 1 ){
-					$("#messageQueue").append(chatInboxTemplate({from: allMessages[i].to_id , name: allMessages[i].to.name, image: allMessages[i].to.picture}));
+					$("#messageQueue").prepend(chatInboxTemplate({from: allMessages[i].to_id , name: allMessages[i].to.name, image: allMessages[i].to.picture}));
 					appendMessages({name: allMessages[i].to.name, from: allMessages[i].to._id});
 					getOutgoingMessage({name: allMessages[i].from.firstName, from: allMessages[i].to._id, image: allMessages[i].from.picture, message:allMessages[i].message, timestamp: allMessages[i].time});
 					$('.chatWindow[data-id='+allMessages[i].to._id+']').closest('.touristChatBox').hide();
 					console.log("hey")
 			}
 			else{
+				$('#messageQueue').prepend($('.chatSidebar[data-id='+allMessages[i].to._id+']').closest('.messageQueueDesign'))
 				getOutgoingMessage({name: allMessages[i].from.firstName, from: allMessages[i].to._id, image: allMessages[i].from.picture, message:allMessages[i].message, timestamp: allMessages[i].time});
 				$('.chatBody').scrollTop($('.chat').height());
 				console.log("hey bud")
